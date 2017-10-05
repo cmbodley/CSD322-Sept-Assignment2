83e80e9ee2370ea89b4de66a6f8ecd55fce3ef4e
IASGROUP\15005472 <michael.blanchard@saultcollege.ca>
2017-10-05 17:38:05 -0400

    Lab2 Not Working

---
+++ F17_csd322_MichaelBlanchard_Lab2/src/java/com/corejsf/userBean.java
@@ -0,0 +1,61 @@
+package com.corejsf;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.inject.Named; 
+import javax.enterprise.context.SessionScoped; 
+
+@Named("user") // or @ManagedBean(name="user")
+@SessionScoped
+public class userBean implements Serializable {
+    
+   private String name;
+   private String address;
+   private Date date;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    public String submit(){
+        return "success";
+    }
+}
